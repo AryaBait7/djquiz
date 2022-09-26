d32735bb2f0471d80bfedeb7335bda6510ed719d
arya <deepabait26577@gmail.com>
2022-09-26 21:06:25 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+
+
+import { useState } from "react";
+import { withAxios } from "react-axios";
+import { BrowserRouter, Route ,Routes} from 'react-router-dom'; // in V6 npm dom uses routes and not switch
 import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Pages/Home';
+import Quiz from './Pages/Quiz';
+import Results from './Pages/Results';
 
 function App() {
+  const [questions, setQuestions] = useState();
+  const [name, setName] = useState();
+  const [score, setScore] = useState(0);
+
+  const fetchQuestions = async (category = "", difficulty = "") => {
+    const { data } = await  withAxios.get(
+      `https://opentdb.com/api.php?amount=10${
+        category && `&category=${category}`
+      }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
+    );
+
+    setQuestions(data.results);
+  };
+   
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div className="app" style={{backgroundImage:"url(https://cdn.pixabay.com/photo/2017/08/31/13/46/blue-color-2700805__340.jpg)"}}>
+      
+       <Header/>
+         <Routes>
+         <Route path='/' element={<Home name={name} setName={setName} fetchQuestions={fetchQuestions}/>} />
+         <Route path='/quiz' element={<Quiz name={name} questions={questions} score={score} setScore={setScore} setQuestions={setQuestions}/>} />
+         <Route path='/results' element={<Results name={name} score={score}/>} />
+
+         </Routes>
+       
     </div>
+    
+    
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>By Arya 
+        <a  href="#">
+           
+        </a>
+    </div>
+  )
+}
+
+export default Footer
